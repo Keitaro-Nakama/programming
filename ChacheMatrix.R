1da44c3607f61c88037e366ff1aba6574d239dc3
Keitaro-Nakama <45575500+Keitaro-Nakama@users.noreply.github.com>
2018-12-05 00:12:06 +0900

    Add files via upload

---
+++ ChacheMatrix.R
@@ -0,0 +1,30 @@
+makeCacheMatrix <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
+
+
+cacheSolve <- function(x, ...) {
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+}
+
+test <- cbind(c(3,2,1), c(5,2,4))
+makeCacheMatrix((test))
+cacheSolve(test)
